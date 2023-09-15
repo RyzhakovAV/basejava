edb1964834ea15f53fe978a32f4af838025f2994
RyzhakovAV <rjizhakov@mail.ru>
2023-09-15 22:47:28 +0500

    homework 1, task in progress

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,45 @@
+import java.util.Arrays;
+
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if ((String.valueOf(storage[i])).equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] copyArray;
+        copyArray = Arrays.copyOf(storage, size);
+        return copyArray;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
