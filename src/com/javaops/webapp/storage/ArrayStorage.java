cf245dd7d1770e8179f942dc6e53694a1b387ac4
RyzhakovAV <rjizhakov@mail.ru>
2023-09-25 07:40:33 +0500

    modified homework 1, start homework 2

--- src/com/javaops/webapp/storage/ArrayStorage.java
+++ src/com/javaops/webapp/storage/ArrayStorage.java
@@ -1,3 +1,7 @@
+package com.javaops.webapp.storage;
+
+import com.javaops.webapp.model.Resume;
+
 import java.util.Arrays;
 
 
@@ -5,34 +9,34 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear() {
+    public void clear() {
         for(int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[size] = r;
         size++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if ((String.valueOf(storage[i])).equals(uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = -1;
         for (int i = 0; i < size; i++) {
-            if((String.valueOf(storage[i])).equals(uuid)) {
+            if(uuid.equals(storage[i].getUuid())) {
                 index = i;
                 i = size;
             }
@@ -50,13 +54,13 @@ public class ArrayStorage {
     }
 
 
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] copyArray;
         copyArray = Arrays.copyOf(storage, size);
         return copyArray;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
