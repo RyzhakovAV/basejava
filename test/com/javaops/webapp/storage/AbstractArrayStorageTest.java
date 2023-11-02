aa96d56d16756c767770fc17af6605293d4b81b2
RyzhakovAV <rjizhakov@mail.ru>
2023-11-02 10:22:57 +0500

    homework4 reflection

---
+++ test/com/javaops/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,38 @@
+package com.javaops.webapp.storage;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AbstractArrayStorageTest {
+
+    @Before
+
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void size() {
+    }
+}
