db38944f4438c3b764debd92044de24f3313fc51
RyzhakovAV <rjizhakov@mail.ru>
2024-01-13 09:51:56 +0500

    start homework 5, abstractStorage

---
+++ src/com/javaops/webapp/storage/AbstractStorage.java
@@ -0,0 +1,53 @@
+package com.javaops.webapp.storage;
+
+import com.javaops.webapp.exception.ExistStorageException;
+import com.javaops.webapp.exception.NotExistStorageException;
+import com.javaops.webapp.model.Resume;
+
+public abstract class AbstractStorage implements Storage {
+    protected abstract boolean isExist(Object searchKey);
+    protected abstract void doUpdate(Resume resume, Object searchKey);
+    protected abstract void doSave(Resume resume, Object searchKey);
+    protected abstract void doDelete(Object searchKey);
+    protected abstract Resume doGet(Object searchKey);
+    protected abstract int getSearchKey(String uuid);
+
+    public void update(Resume resume) {
+        Object searchKey = getExistedSearchKey(resume.getUuid());
+        doUpdate(resume, searchKey);
+    }
+
+    public void save(Resume resume) {
+        Object searchKey = getNotExistedSearchKey(resume.getUuid());
+        doSave(resume, searchKey);
+
+    }
+
+    public void delete(String uuid) {
+        Object searchKey = getExistedSearchKey(uuid);
+        doDelete(searchKey);
+    }
+
+    public Resume get(String uuid) {
+        Object searchKey = getExistedSearchKey(uuid);
+        return doGet(searchKey);
+    }
+
+
+
+    private Object getExistedSearchKey (String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (!isExist(searchKey)) {
+            throw new NotExistStorageException(uuid);
+        }
+        return searchKey;
+
+    }
+    private Object getNotExistedSearchKey (String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (isExist(searchKey)) {
+            throw new ExistStorageException(uuid);
+        }
+        return searchKey;
+    }
+}
